fe747160a39f525a2498299ceff9bbac24920102
Flavio Rocha <flavio_271295@hotmail.com>
2021-06-09 15:43:54 -0300

    test prettier config

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,14 +1,21 @@
-import useAuth from "../hooks/useAuth";
+import useAuth from '../hooks/useAuth';
 
-function Dashboard(props) {
+// eslint-disable-next-line no-unused-vars
+function Dashboard(_props) {
   const { user } = useAuth();
   console.log(user);
 
-  return (<div>
+  if (true) {
+    useState();
+  }
+  return (
+    <div>
+      <a href="" className="test" target="_blank" rel="test"></a>
       <img src={user?.photoURL} alt="" />
-    <h1>Name: {user?.displayName}</h1>
-    <h1>Email: {user?.email}</h1>
-  </div>);
+      <h1>Name: {user?.displayName}</h1>
+      <h1>Email: {user?.email}</h1>
+    </div>
+  );
 }
 
 export default Dashboard;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
-import useAuth from "../hooks/useAuth";
-
+import useAuth from '../hooks/useAuth';
+import { ChakraProvider } from '@chakra-ui/react';
+// test
 export default function Home() {
   const { user, signin } = useAuth();
-  console.log(`user ${user}`); 
+  console.log(`user ${user}`);
   return (
     <div>
       <h1>Initial</h1>
